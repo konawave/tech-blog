7ffca096e589647dc65fd8725112897bf74ec679
konawave <nickkjeffs@gmail.com>
2023-08-12 09:16:56 -0400

    anotha one

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,21 +4,22 @@ const { User, Post } = require('../models');
 // need to create an array called posts that pushes all the posts from the database
 
 // to render home page
-// router.get('/', async (req, res) => {
-//     try {
-//         let postArr = await Post.findAll();
-//         console.log(postArr);
-//         res.render('home', { postArr });
-//     } catch (err) {
-//         console.log(err, 'No luck getting home!')
-//         res.status(500).json(err);
-//     }
-// });
+router.get('/', async (req, res) => {
+    try {
+        let postArr = await Post.findAll();
+        console.log(postArr);
+        res.render('home', { postArr });
+    } catch (err) {
+        console.log(err, 'No luck getting home!')
+        res.status(500).json(err);
+    }
+});
 
 router.get('/login', async (req, res) => {
     try {
         res.render('login');
     } catch (err) {
+        console.log('could not get to login page')
         res.status(500).json(err);
     }
 });
@@ -27,6 +28,7 @@ router.get('/dash', async (req, res) => {
     try {
         res.render('dashboard');
     } catch (err) {
+        console.log('no luck getting to dash!')
         res.status(500).json(err);
     }
 });
